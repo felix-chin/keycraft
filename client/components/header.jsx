8039e3681b7157ff38c261f29fd9f815a3098ed5
Felix Chin <felixmchin@gmail.com>
2020-07-23 12:09:57 -0700

    added Header component

---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <header>
+      <div className="mx-4 mt-2">
+        <h1>Wicked Sales</h1>
+      </div>
+    </header>
+  );
+}
