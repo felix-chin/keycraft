0541638d5d65cb0944ae98229ac7757ff281d116
Felix Chin <felixmchin@gmail.com>
2020-07-24 10:31:05 -0700

    update to prevent skeleton flash while loading

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -4,7 +4,7 @@ export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: {}
+      product: null
     };
   }
 
@@ -21,25 +21,29 @@ export default class ProductDetails extends React.Component {
   render() {
     const product = this.state.product;
     const setView = this.props.setView;
-    return (
-      <div className="container">
-        <div className="container bg-white border rounded shadow-sm py-3">
-          <div className="row mb-3">
-            <span onClick={() => setView('catalog', {})} className="col-md-auto text-muted ml-2 cursor-pointer">{'< Back to catalog'}</span>
-          </div>
-          <div className="row">
-            <img src={product.image} className="col-md-5 img-fluid" />
-            <div className="col-md-7">
-              <h3>{product.name}</h3>
-              <h5 className="text-muted">{'$' + (product.price / 100).toFixed(2)}</h5>
-              <p>{product.shortDescription}</p>
+    if (!product) {
+      return null;
+    } else {
+      return (
+        <div className="container">
+          <div className="container bg-white border rounded shadow-sm py-3">
+            <div className="row mb-3">
+              <span onClick={() => setView('catalog', {})} className="col-md-auto text-muted ml-2 cursor-pointer">{'< Back to catalog'}</span>
+            </div>
+            <div className="row">
+              <img src={product.image} className="col-md-5 img-fluid" />
+              <div className="col-md-7">
+                <h3>{product.name}</h3>
+                <h5 className="text-muted">{'$' + (product.price / 100).toFixed(2)}</h5>
+                <p>{product.shortDescription}</p>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <p className="col-md-auto">{product.longDescription}</p>
             </div>
           </div>
-          <div className="row mt-3">
-            <p className="col-md-auto">{product.longDescription}</p>
-          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
