c25144a420945116fe65112a3adac97f5bae80b9
Felix Chin <felixmchin@gmail.com>
2020-09-03 17:20:23 -0700

    updated modal styling and added form validation

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -5,9 +5,12 @@ export default class ProductDetails extends React.Component {
     super(props);
     this.state = {
       product: null,
-      quantity: 1
+      quantity: 1,
+      switches: [],
+      switch: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.decreaseQty = this.decreaseQty.bind(this);
     this.increaseQty = this.increaseQty.bind(this);
   }
@@ -16,9 +19,25 @@ export default class ProductDetails extends React.Component {
     const productId = this.props.productId;
     fetch(`/api/products/${productId}`)
       .then(res => res.json())
-      .then(data => this.setState({
-        product: data
-      }))
+      .then(data => {
+        this.setState({
+          product: data
+        });
+        if (this.state.product.switchOptions === 'basic') {
+          this.setState({
+            switches: ['Cherry MX Red', 'Cherry MX Brown', 'Cherry MX Blue', 'Gateron Black', 'Gateron Brown', 'Gateron Green']
+          });
+        } else if (this.state.product.switchOptions === 'premium') {
+          this.setState({
+            switches: ['Cherry MX Green', 'Cherry MX Clear', 'Kailh Box Royal', 'Hako True', 'Hako Violet']
+          });
+        } else {
+          this.setState({
+            switches: ['Topre 30g', 'Topre 35g', 'Topre 45g', 'Topre 55g']
+          });
+        }
+      }
+      )
       .catch(err => console.error(err));
   }
 
@@ -26,6 +45,12 @@ export default class ProductDetails extends React.Component {
     this.setState({
       [event.target.name]: event.target.value
     });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const addToCart = this.props.addToCart;
+    addToCart(product, this.state.switch);
   }
 
   decreaseQty() {
@@ -42,7 +67,7 @@ export default class ProductDetails extends React.Component {
   render() {
     const product = this.state.product;
     const setView = this.props.setView;
-    const addToCart = this.props.addToCart;
+
     if (!product) {
       return null;
     } else {
@@ -59,18 +84,17 @@ export default class ProductDetails extends React.Component {
             </div>
             <div className="row bg-white border rounded shadow-sm py-3">
               <img src={product.image} className="col-md-7 img-fluid" />
-              <div className="col-md-5">
+              <form onSubmit={this.handleSubmit} className="col-md-5">
                 <h3>{product.name}</h3>
                 <h5 className="text-muted">{'$' + (product.price / 100).toFixed(2)}</h5>
                 <p>{product.shortDescription}</p>
                 <div>
                   <select name="switch" value={this.state.value} defaultValue="" onChange={this.handleChange} required>
                     <option value="" disabled>Select a switch</option>
-                    <option value="mx brown">Cherry MX Brown</option>
-                    <option value="mx red">Cherry MX Red</option>
-                    <option value="mx blue">Cherry MX Blue</option>
-                    <option value="mx black">Cherry Mx Black</option>
-                    <option value="mx clear">Cherry MX Clear</option>
+                    { this.state.switches.map((item, i) =>
+                      <option key={i} value={item}>{item}</option>
+                    )
+                    }
                   </select>
                 </div>
                 <div>
@@ -83,12 +107,11 @@ export default class ProductDetails extends React.Component {
                   </span>
                 </div>
                 <button
-                  type="button"
-                  onClick={() => addToCart(product)}
+                  type="submit"
                   className="btn btn-color">
                     Add to Cart
                 </button>
-              </div>
+              </form>
             </div>
           </div>
         </section>
