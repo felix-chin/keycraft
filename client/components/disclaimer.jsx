64b8e5d04b8d5ac95d640997a47ec1c3002eb103
Felix Chin <felixmchin@gmail.com>
2020-09-05 23:38:32 -0700

    changed styling for disclaimer component

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -6,8 +6,8 @@ export default function Disclaimer(props) {
   return (
     <section className="modal-overlay d-flex">
       <div className="disclaimer-content text-white p-3">
-        <p>This application is for demonstration purposes only and does not allow for real world transactions.<br />
-          By clicking the button below, you acknowledge that you have read and understand this disclaimer.</p>
+        <p>This application is for demonstration purposes only and does not allow for real world transactions.</p>
+        <p>By clicking the button below, you acknowledge that you have read and understand this disclaimer.</p>
         <button onClick={props.closeDisclaimer} className="btn btn-color">
           Acknowledge
         </button>
